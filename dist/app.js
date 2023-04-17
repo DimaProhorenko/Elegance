e585fabf661b8e2b3593b4ce9fe90c6ddaa661fc
DimaProhorenko <dima.study.web@gmail.com>
2023-04-17 19:55:36 +0300

    collapse element

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,48 @@
 const form = document.querySelector('.form');
-const requiredInputs = form.querySelectorAll('.form__control:required');
 
-const addOnlyUniqueClass = (el, className) => {
-    if (el.classList.contains(className)) return;
-    el.classList.add(className);
+if (form) {
+    const requiredInputs = form.querySelectorAll('.form__control:required');
+
+    const addOnlyUniqueClass = (el, className) => {
+        if (el.classList.contains(className)) return;
+        el.classList.add(className);
+    }
+
+    requiredInputs.forEach(input => {
+        input.addEventListener('blur', (e) => {
+            addOnlyUniqueClass(e.target, 'blurred');
+        })
+    })
+    console.log(requiredInputs);
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+    })
 }
 
-requiredInputs.forEach(input => {
-    input.addEventListener('blur', (e) => {
-        addOnlyUniqueClass(e.target, 'blurred');
-    })
-})
-console.log(requiredInputs);
+const collapseElList = document.querySelectorAll('.collapse');
+const collapseTogglers = document.querySelectorAll('[data-ek-toggle]');
 
-form.addEventListener('submit', e => {
-    e.preventDefault();
-})
+
+collapseToggleHandler = e => {
+    if (e.target.tagName === 'A') e.preventDefault();
+
+    const target = e.target.dataset.ekTarget || '#' + e.target.href.split('#')[1];
+
+    const targetCollapse = document.querySelector(target);
+    
+    if (targetCollapse.classList.contains('show')) {
+        targetCollapse.classList.remove('show');
+        targetCollapse.style.maxHeight = null;
+    } else {
+        targetCollapse.classList.add('show');
+        targetCollapse.style.maxHeight = targetCollapse.scrollHeight + 'px';
+    }
+
+    
+}
+
+
+if (collapseTogglers) {
+    collapseTogglers.forEach(toggler => toggler.addEventListener('click', collapseToggleHandler));
+}
